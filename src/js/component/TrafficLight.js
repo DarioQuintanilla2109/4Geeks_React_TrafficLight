6bbebc0c741526df0d8d8c410825b6d2036a7b53
DarioQuintanilla2109 <dquin106@fiu.edu>
2020-09-09 23:32:10 +0000

    updated to function

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,45 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class TrafficLight extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			clickedLight: null
-		};
-	}
-
-	render() {
-		let redLight = "";
-		if (this.state.clickedLight == "red") redLight = "selected";
-		let yellowLight = "";
-		if (this.state.clickedLight == "yellow") yellowLight = "selected";
-		let greenLight = "";
-		if (this.state.clickedLight == "green") greenLight = "selected";
-		return (
-			<div>
-				<div id="trafficTop" />
-				<div id="container">
-					<div
-						className={"red light " + redLight}
-						onClick={() => this.setState({ clickedLight: "red" })}>
-						{" "}
-					</div>
-					<div
-						className={"yellow light " + yellowLight}
-						onClick={() =>
-							this.setState({ clickedLight: "yellow" })
-						}>
-						{" "}
-					</div>
-					<div
-						className={"green light " + greenLight}
-						onClick={() =>
-							this.setState({ clickedLight: "green" })
-						}>
-						{" "}
-					</div>
+export const TrafficLight = () => {
+	const [clickedLight, setClickedLight] = useState(null);
+	let redLight = "";
+	if (clickedLight == "red") redLight = "selected";
+	let yellowLight = "";
+	if (clickedLight == "yellow") yellowLight = "selected";
+	let greenLight = "";
+	if (clickedLight == "green") greenLight = "selected";
+	return (
+		<div>
+			<div id="trafficTop" />
+			<div id="container">
+				<div
+					className={"red light " + redLight}
+					onClick={() => setClickedLight("red")}>
+					{" "}
+				</div>
+				<div
+					className={"yellow light " + yellowLight}
+					onClick={() => setClickedLight("yellow")}>
+					{" "}
+				</div>
+				<div
+					className={"green light " + greenLight}
+					onClick={() => setClickedLight("green")}>
+					{" "}
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-import TrafficLight from "./component/TrafficLight";
+import { TrafficLight } from "./component/TrafficLight";
 //import your own components
 import { Home } from "./component/home.js";
 
